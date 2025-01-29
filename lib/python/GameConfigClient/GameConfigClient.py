2eddce500aed1fd1219f19689294dea71d054998
Adam Connolly <45522351+aconn1994@users.noreply.github.com>
2025-01-29 07:52:19 -0500

    add parser util for arma 3 mods

--- lib/python/GameConfigClient/GameConfigClient.py
+++ lib/python/GameConfigClient/GameConfigClient.py
@@ -5,7 +5,7 @@ class GameConfigClient:
     def __init__(self, local_game_dir) -> None:
         self.local_game_dir: str = local_game_dir
         self.config: str = None
-        self.workshop_items_list: str = None
+        self.workshop_items_dict: dict[str, str] = None
         self._execute()
 
     def _get_configuration(self) -> dict[str, dict[str, str]]: # Convert to Python Model/Yaml File pattern?
@@ -21,5 +21,11 @@ class GameConfigClient:
         self.config = self._get_configuration()[self.local_game_dir]
         game_utils = importlib.import_module(f'{self.local_game_dir}.utils')
 
-        if self.local_game_dir == 'arma_three':
-            parsed_mod_list = game_utils.parse_mod_file(self.config['mod_file_path'])
+        if self.local_game_dir == 'arma_three' and os.path.exists(self.config['mod_file_path']):
+            self.workshop_items_dict = game_utils.parse_mod_file(self.config['mod_file_path'])
+
+    def handle_mod_configuration(
+        self,
+        steam_client,
+    ) -> None:
+        pass
--- lib/python/arma_three/utils.py
+++ lib/python/arma_three/utils.py
@@ -1,16 +1,33 @@
 from html.parser import HTMLParser
 
-class MyHTMLParser(HTMLParser): # TO BE CONTINUED.............
-    def handle_starttag(self, tag, attrs):
-        print("Encountered a start tag:", tag)
+class ArmaThreeHTMLParser(HTMLParser): # TO BE CONTINUED.............
+    def __init__(self):
+        super(ArmaThreeHTMLParser, self).__init__()
+        self.html_as_dict = {}
+        self.start_tag = None
+        self.is_display_name = False
+        self.display_name = None
 
-    def handle_endtag(self, tag):
-        print("Encountered an end tag: ", tag)
+    def handle_starttag(self, tag, attrs):
+        if tag == 'td':
+            for attr in attrs:
+                if attr[0] == 'data-type' and attr[1] == 'DisplayName':
+                    self.start_tag = tag
+                    self.is_display_name = True
+        elif tag == 'a':
+            self.start_tag = tag
 
     def handle_data(self, data):
-        print("Encountered some data: ", data)
+        if self.start_tag == 'td' and self.is_display_name:
+            self.display_name = data
+            self.html_as_dict[self.display_name] = None
+            self.is_display_name = False
+        elif self.start_tag == 'a' and self.display_name:
+            self.html_as_dict[self.display_name] = data.split('=')[1]
+            self.display_name = None
 
 def parse_mod_file(mod_file) -> None:
-    html_parser = MyHTMLParser()
+    html_parser = ArmaThreeHTMLParser()
     mf = open(mod_file, "r")
     html_parser.feed(mf.read())
+    return html_parser.html_as_dict
