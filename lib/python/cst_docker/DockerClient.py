1521ee36bfb762d70da2356ab0654e9c40f6d3b7
Adam Connolly <45522351+aconn1994@users.noreply.github.com>
2025-01-31 08:27:51 -0500

    build cleanup into docker client

--- lib/python/cst_docker/DockerClient.py
+++ lib/python/cst_docker/DockerClient.py
@@ -1,23 +1,54 @@
 import subprocess
 
 class DockerClient:
-    def __init__(self):
-        self.docker_binary = ['docker']
+    def __init__(self, workspace_name: str, exists: bool = False):
+        self.workspace_name: str = workspace_name
+        self.exists: bool = exists
 
-    def help(self) -> None:
-        docker_cmd = self.docker_binary
-        docker_cmd.extend(['--help'])
+        self.container_id: str | None = None
+        self.image_id: str | None = None
+
+        if exists:
+            self._get_container_id()
+            self._get_image_id()
+
+    def _get_container_id(self) -> None:
+        docker_cmd = ['docker']
+        docker_cmd.extend(['ps', '-aqf', f'name={self.workspace_name}'])
+        self.container_id = subprocess.run(docker_cmd, capture_output=True, text=True).stdout.strip()
+
+    def _get_image_id(self) -> None:
+        docker_cmd = ['docker']
+        docker_cmd.extend(['images', '-q', self.workspace_name])
+        self.image_id = subprocess.run(docker_cmd, capture_output=True, text=True).stdout.strip()
+
+    def remove_container(self, force: bool = False) -> None:
+        print(f'Cleaning up {self.workspace_name} container...')
+        docker_cmd = ['docker']
+        docker_cmd.extend(['container', 'rm', self.container_id])
+        if force:
+            docker_cmd.extend('--force')
+        print(f'{self.workspace_name} container has been removed.')
+
+    def remove_image(self, force: bool = False) -> None:
+        print(f'Cleaning up {self.workspace_name} image...')
+        docker_cmd = ['docker']
+        docker_cmd.extend(['rmi', self.image_id])
+        if force:
+            docker_cmd.extend('--force')
+        subprocess.call(docker_cmd)
+        print(f'{self.workspace_name} image has been removed.')
+
+    @staticmethod
+    def prune(entity_type: str, all_entities: bool = False, force: bool = False) -> None:
+        docker_cmd = ['docker']
+        docker_cmd.extend([entity_type, 'prune'])
+        if all_entities:
+            docker_cmd.extend(['-a'])
+        if force:
+            docker_cmd.extend(['-f'])
         subprocess.call(docker_cmd)
 
-    def get_image(self, game_image_name: str) -> str:
-        docker_cmd = self.docker_binary
-        docker_cmd.extend(['images', '-q', game_image_name])
-        return subprocess.run(docker_cmd, capture_output=True, text=True).stdout.strip()
-
-
-    def remove_game_image(self):
-        docker_cmd = self.docker_binary
-        docker_cmd.extend(['rmi', "$(docker images 'game-server' -q)"])
-
-        subprocess.check_call(docker_cmd)
-        subprocess.call(docker_cmd)
+    def build_image(self) -> None:
+        # todo, use yaml as config???
+        pass
--- lib/python/cst_docker/build.py
+++ lib/python/cst_docker/build.py
@@ -1,9 +1,28 @@
+import argparse
 from DockerClient import DockerClient
 
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '-c',
+    '--clean-up',
+    help='Clean up local docker entities',
+    action='store_true'
+)
+
 def main():
-    docker = DockerClient()
-    game_image = docker.get_image('game-server')
-    print(game_image)
+    args = parser.parse_args()
+    workspace = 'game-server'
+
+    docker = DockerClient(workspace, exists=args.clean_up)
+
+    if args.clean_up:
+        docker.remove_container(force=True)
+        docker.remove_image(force=True)
+        docker.prune('container', force=True)
+        docker.prune('image', force=True)
+        docker.prune('volume', force=True)
+
+    docker.build_image()
 
 if __name__ == '__main__':
     main()
