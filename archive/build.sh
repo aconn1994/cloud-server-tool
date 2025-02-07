786bebac16cff948c81f32e80d54188b3ea3cc02
Adam Connolly <45522351+aconn1994@users.noreply.github.com>
2025-02-07 15:32:43 -0500

    remove archive code

--- archive/build.sh
+++
@@ -1,9 +0,0 @@
-# Remove entities to be replaced
-docker container rm game-server
-docker rmi $(docker images 'game-server' -q)
-
-# Rebuild docker image and run
-docker image prune -f
-docker volume prune -f
-docker build -t game-server:latest --build-arg STEAM_USERNAME_ARG=$1 --build-arg STEAM_PASSWORD_ARG=$2 .
-docker run --name game-server -p 2302:2302/udp -p 2303:2303/udp -p 2304:2304/udp -p 2305:2305/udp -p 2306:2306/udp -v /Users/adamconnolly/dev/cloud-server-tool/lib/python/cst_game:/home/gameuser/cst_game -it game-server
--- archive/handle_mods.sh
+++
@@ -1,2 +0,0 @@
-cd steamcmd/arma3/steamapps/workshop/content
-find . -depth -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \;
