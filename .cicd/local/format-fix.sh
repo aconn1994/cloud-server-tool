933d8aca3ff35d959c71e6a43e5ea5c48c524d48
Adam Connolly <45522351+aconn1994@users.noreply.github.com>
2025-05-02 15:13:47 -0400

    update testing action

--- .cicd/local/format-fix.sh
+++ .cicd/local/format-fix.sh
@@ -9,4 +9,4 @@ set -euo pipefail
 #/bin/bash .cicd/bash/linters/python-format.sh .cicd/python lib/python/cst cst "" lib/python/cst/pyproject.toml /root/venvs/cst/bin/python3
 
 # cloud_server_tool
-/bin/bash .cicd/bash/linters/python-format.sh .cicd/python lib/python/cloud_server_tool cloud_server_tool "" lib/python/cloud_server_tool/pyproject.toml /root/venvs/cloud_server_tool/bin/python3
+/bin/bash .cicd/bash/linters/python-format.sh lib/python/cloud_server_tool lib/python/cloud_server_tool cloud_server_tool "" lib/python/cloud_server_tool/pyproject.toml /root/venvs/cloud_server_tool/bin/python3
--- .cicd/local/test_python_lib_w_coverage.sh
+++ .cicd/local/test_python_lib_w_coverage.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # CICD
-#/bin/bash .cicd/bash/testers/coverage-pytest.sh .cicd/python/cst_cicd .cicd/python/cst_cicd cst_cicd pyproject.toml python3 # todo, need to fix
+#/bin/bash .cicd/bash/testers/coverage-pytest.sh .cicd/python .cicd/python cst_cicd pyproject.toml python3 # todo, need to fix
 
 # cloud_server_tool
 /bin/bash .cicd/bash/testers/coverage-pytest.sh lib/python/cloud_server_tool lib/python/cloud_server_tool cloud_server_tool pyproject.toml /root/venvs/cloud_server_tool/bin/python3
