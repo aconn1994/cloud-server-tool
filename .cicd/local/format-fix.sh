efd39e61a0516d46db928ef930c79f4c8891b923
Adam Connolly <45522351+aconn1994@users.noreply.github.com>
2026-06-11 09:57:01 -0400

    update dockerfile

--- .cicd/local/format-fix.sh
+++ .cicd/local/format-fix.sh
@@ -3,10 +3,7 @@
 set -euo pipefail
 
 # CICD
-/bin/bash .cicd/bash/linters/python-format.sh .cicd/python .cicd/python cicd "" .cicd/python/pyproject.toml /root/venvs/cicd/bin/python3
+/bin/bash .cicd/bash/linters/python-format.sh .cicd/python .cicd/python cicd "" .cicd/python/pyproject.toml /usr/local/bin/python3
 
 # CST
-#/bin/bash .cicd/bash/linters/python-format.sh .cicd/python lib/python/cst cst "" lib/python/cst/pyproject.toml /root/venvs/cst/bin/python3
-
-# cst
 /bin/bash .cicd/bash/linters/python-format.sh lib/python/cst lib/python/cst cst "" lib/python/cst/pyproject.toml /root/venvs/cst/bin/python3
--- .cicd/local/test_python_lib_w_coverage.sh
+++ .cicd/local/test_python_lib_w_coverage.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # CICD
-#/bin/bash .cicd/bash/testers/coverage-pytest.sh .cicd/python .cicd/python cst_cicd pyproject.toml python3 # todo, need to fix
+#/bin/bash .cicd/bash/testers/coverage-pytest.sh .cicd/python .cicd/python cst_cicd pyproject.toml /usr/local/bin/python3
 
 # cst
 /bin/bash .cicd/bash/testers/coverage-pytest.sh lib/python/cst lib/python/cst cst pyproject.toml /root/venvs/cst/bin/python3
